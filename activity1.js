52a439358d2a71073dafdea7a0990e594ed1ef8d
issaRonin <101780577+issaRonin@users.noreply.github.com>
2022-09-26 19:35:27 -0700

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,14 @@
+score = 0;
+ function updatescore()
+{
+    score = score + 1;
+    document.getElementById('score').innerHTML= "Score: " + score;
+}
+function savescore()
+{
+    localStorage.setItem("score", score);
+}
+function nextpage()
+{
+    window.location = "newpage.html";
+}
